4ccdba74b2649e52b6fc9bbef64bbd31f3c72ff9
Trương Thanh Tùng <truongthanhtung030502@gmail.com>
2024-07-11 02:04:42 +0700

    code

--- app/src/main/java/com/example/myapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/myapp/Activities/MainActivity.java
@@ -13,6 +13,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapp.Activities.database.DBManager;
+import com.example.myapp.Activities.ui.usershome.Register;
 import com.example.myapp.Activities.ui.usershome.userHomeActivity;
 import com.example.myapp.R;
 
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+//
+//        DBManager dbManager=new DBManager(this);
+//        dbManager.open();
 
         // Init
         bt_signIn = findViewById(R.id.bt_signin);
@@ -41,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v) {
                Intent a =new Intent(MainActivity.this, userHomeActivity.class);
                startActivity(a);
+           }
+       });
+       bt_Register.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(MainActivity.this, Register.class);
+               startActivity(intent);
            }
        });
     }
